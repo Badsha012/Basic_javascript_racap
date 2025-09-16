d9baf6bf2e00d63d44bc775a9e39a5353a99781d
Badsha012 <badshasheikh361@gmail.com>
2025-09-16 20:40:19 +0600

    two commit

--- array.js
+++ array.js
@@ -1,7 +1,9 @@
 const friends=["Hero","HR","SRK","Badsha"];
 
-friends.push("Pd")
+// friends.push("Pd")
 
-friends.pop("Badsha")
-console.log (friends);
+// friends.pop("Badsha")
+// frist jok korar janni
+friends.unshift("Bd")
+console.log (friends.slice(2,3));
 console.log(friends[2]);
