84b373d2562618809f4a7d7db0f2b8e3a80a56e6
Badsha012 <badshasheikh361@gmail.com>
2025-09-16 20:26:14 +0600

    six commit

---
+++ if_else.js
@@ -0,0 +1,7 @@
+const num=20
+if(num>=20 && num <30){
+    console.log("yes");
+}
+else{
+    console.log("No");
+}
--- variable.js
+++ variable.js
@@ -1,1 +1,5 @@
 const x=6;
+let name="Badsha";
+name="Badsha Sheikh";
+
+const countryName="BD";
