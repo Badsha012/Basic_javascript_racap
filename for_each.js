90168616e2f36af3210dd13ff310978e48f2d02a
Badsha012 <badshasheikh361@gmail.com>
2025-09-17 09:56:07 +0600

    ten commit

---
+++ for_each.js
@@ -0,0 +1,9 @@
+const product=[
+    {name:"redmi",brand:"xiami",price:40000 ,colo:"Green"},
+     {name:"sas",brand:"sansume",price:30000 ,colo:"Black"},
+      {name:"Iphne",brand:"Apple",price:140000 ,colo:"White"},
+       {name:"Vivo",brand:"Vivo",price:44000 ,colo:"Blue"},
+
+]
+// for each is no element k return kora na
+product.forEach(product => console.log(product.price));
